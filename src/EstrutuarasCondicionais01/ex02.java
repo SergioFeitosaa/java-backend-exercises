ced92be26fca5b9eb14636c213ca87f4eaf967e2
SergioFeitosaa <sergio.feitosa2@gmail.com>
2025-11-17 22:58:37 -0300

    Initial commit

---
+++ src/EstrutuarasCondicionais01/ex02.java
@@ -0,0 +1,28 @@
+package EstrutuarasCondicionais01;
+  /*
+    Jéssica trabalha no setor de TI de uma empresa e precisa garantir a segurança dos acessos ao sistema interno.
+    Os funcionários utilizam uma senha fixa para acessar suas contas, e o sistema deve verificar se a senha inserida está correta.
+     Ajude Jéssica a criar um programa que, com base em uma senha pré-definida, por exemplo: 123456, verifique se o que o usuário digitou está correto ou não.
+     O programa deve comparar a tentativa com a senha correta e exibir se o acesso foi permitido ou negado.
+     */
+import java.util.Scanner;
+
+public class ex02 {
+    public static void main(String[] args) {
+        String senhaCorreta = "123456";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a senha: ");
+        String tentativaSenha = scanner.nextLine();
+
+        scanner.close();
+
+        if (tentativaSenha.equals(senhaCorreta)){
+            System.out.println("Acesso permitido!");
+        }else {
+            System.out.println("Acesso negado!");
+        }
+
+
+    }
+}
---
+++ src/EstrutuarasCondicionais01/ex03.java
@@ -0,0 +1,33 @@
+package EstrutuarasCondicionais01;
+/*
+    Pedro está aprendendo Java e se deparou com um problema: ele precisa criar um programa que
+    compare dois números inteiros fornecidos pelo usuário e informe qual é o maior ou se são iguais.
+    Mas, ele está com dificuldades para implementar a lógica de comparação e exibir o resultado
+    corretamente.
+
+    Ajude Pedro a resolver esse problema! Crie um programa que solicite ao usuário dois números inteiros,
+    compare-os e exiba uma mensagem indicando qual é o maior ou se ambos são iguais.
+ */
+
+import java.util.Scanner;
+
+public class ex03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Digite o segundo número: ");
+        int num2 = scanner.nextInt();
+
+        if (num1 > num2) {
+            System.out.println("O maior número é " + num1 + ".");
+        } else if (num2 > num1) {
+            System.out.println("O maior número é " + num2 + ".");
+        }else {
+            System.out.println("Os números sáo iguais");
+        }
+    }
+
+}
